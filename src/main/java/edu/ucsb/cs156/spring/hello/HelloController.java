3c9d269a4562b5f9552adfa83f7c00470b2d46d8
KaneD985 <ydeng672@ucsb.edu>
2023-04-19 19:34:40 -0700

    Kane- modify the test to make it work

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from ydeng672";
+        return "Greetings from ydeng672!";
     }
     
 }
